dee2a2f2b4847843a8a29bc7b5ef19d625f68756
Aguira1908 <zikkriamri123@gmail.com>
2025-12-13 14:17:59 +0700

    first commit: Setup Project & Folder

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeLayout from '../Layouts/HomeLayout';
+import HeroSection from '../Components/HeroSection';
+
+const Home = () => {
+  return (
+    <HomeLayout>
+      <HeroSection />
+    </HomeLayout>
+  );
+};
+
+export default Home;
