b6efdd08c02135acea4a507146c3aae4662fb2b6
Aguira1908 <zikkriamri123@gmail.com>
2025-12-14 13:36:00 +0700

    add hero transition

--- resources/js/Components/Transition/HeroTransition.jsx
+++ resources/js/Components/Transition/HeroTransition.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const HeroTransition = () => {
   return (
-    <div className="w-full min-h-screen text-white text-6xl flex items-center justify-center font-bold ">
-      HeroTransition
-    </div>
+    <section className="w-full my-[10%] min-h-screen text-white text-xl xl:text-6xl md:text-3xl font-bold  ">
+      <div className="h-screen w-full leading-loose tracking-wide flex flex-col items-start justify-center p-4 lg:p-12">
+        <h1>So, You Scrolling?</h1>
+        <h2>Okay Okayy... Let me tell you something</h2>
+      </div>
+    </section>
   );
 };
 
